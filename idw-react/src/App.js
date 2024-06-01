c8ac537a594e8cbba1b9806bbafbe7958abf3a0c
NaGi1611 <petty1611@hotmail.com>
2024-06-01 18:13:04 -0300

    footer y nosotros

--- idw-react/src/App.js
+++ idw-react/src/App.js
@@ -2,7 +2,8 @@
 import { BrowserRouter,  Route, Routes } from "react-router-dom";
 import Nav from "./components/Nav";
 import AddAlojamientoForm from "./components/AddAlojamiento";
-import GetAlojamientos from "./components/GetAlojamientos"
+import GetAlojamientos from "./components/GetAlojamientos";
+import Footer from "./components/Footer";
 // import Home from "./components/Home";
 
 function App() {
@@ -11,14 +12,16 @@ function App() {
     <BrowserRouter>
     <div>
       <Nav/>
+      <Footer/>
     </div>
+
     <Routes>
           {/* <Route path="/" element={<Home/>}/> */}
           <Route path="/agregaralojamiento" element={<AddAlojamientoForm/>}/>
           <Route path="/alojamientos" element={<GetAlojamientos/>}/>
     </Routes>
     </BrowserRouter>
-    
+   
   );
 }
 
