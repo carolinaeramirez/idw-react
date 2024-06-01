e61f8a6ba9484014d65e2522e7b165c491f29d61
carolinaeramirez <crmz4836@gmail.com>
2024-05-31 21:50:59 -0300

    actualizo

--- idw-react/src/App.js
+++ idw-react/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter,  Route, Routes } from "react-router-dom";
 import Nav from "./components/Nav";
 import AddAlojamientoForm from "./components/AddAlojamiento";
 import GetAlojamientos from "./components/GetAlojamientos"
-// import Home from "./components/Home";
+import Home from "./pages/Home";
 
 function App() {
   return (
@@ -13,9 +13,9 @@ function App() {
       <Nav/>
     </div>
     <Routes>
-          {/* <Route path="/" element={<Home/>}/> */}
+          <Route path="/" element={<Home/>}/>
           <Route path="/agregaralojamiento" element={<AddAlojamientoForm/>}/>
-          <Route path="/alojamientos" element={<GetAlojamientos/>}/>
+        
     </Routes>
     </BrowserRouter>
     
