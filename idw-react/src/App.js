ecb8cb55d465690e01eb39778288802765607cdd
NaGi1611 <petty1611@hotmail.com>
2024-05-31 23:44:36 -0300

    mejoras

--- idw-react/src/App.js
+++ idw-react/src/App.js
@@ -2,8 +2,10 @@
 import { BrowserRouter,  Route, Routes } from "react-router-dom";
 import Nav from "./components/Nav";
 import AddAlojamientoForm from "./components/AddAlojamiento";
-import GetAlojamientos from "./components/GetAlojamientos"
+import AddTipoAlojamiento from "./components/AddTipoAlojamiento";
+import GetAlojamiento from "./components/GetTipoAlojamiento";
 import Home from "./pages/Home";
+
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
     <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/agregaralojamiento" element={<AddAlojamientoForm/>}/>
+          <Route path="/agregartipoalojamiento" element={<AddTipoAlojamiento/>}/>
+          <Route path="/vertipoalojamiento" element={<GetAlojamiento/>}/>          
     </Routes>
     </BrowserRouter>
   );
--- idw-react/src/components/GetTipoAlojamiento.jsx
+++ idw-react/src/components/GetTipoAlojamiento.jsx
@@ -1,19 +1,43 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export default function GetTipoAlojamiento() {
-    const {tipoAlojamiento, setTipoAlojamiento}=useState([]);
-    const {error, setError}=useState("");
+    const [tipoAlojamiento, setTipoAlojamiento] = useState([]);
+    const [error, setError] = useState("");
 
-    const traerTipoAlojamiento =async ()=>{
+    const traerTipoAlojamiento = async () => {
         try {
-            const response= await axios.get()
+            const response = await axios.get("http://localhost:3001/tiposAlojamiento/getTiposAlojamiento");
+            console.log(response, "Respuesta");           
+            setTipoAlojamiento(response.data);
         } catch (error) {
-            
+            setError("No se pudo cargar la información solicitada");
         }
-    }
-  return (
-    <div>
-      
-    </div>
-  )
+    };
+
+    useEffect(() => {
+        traerTipoAlojamiento();
+    }, []);
+
+    console.log("array tipoAlojamiento:", tipoAlojamiento);  
+
+    return (
+        <div>
+            {error && <p>{error}</p>}
+            <table className="table">
+                <thead className="tableTitle">
+                    <tr className="tr">
+                        <th>Tipo de Alojamientos cargados</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {tipoAlojamiento.map((tipo, index) => (
+                        <tr key={index}>
+                            <td>{tipo.descripcion}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
 }
